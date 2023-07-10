e60439346065069d83a3f98e8319f1fcf69596e2
catzilla <catzillaorz@outlook.com>
2023-07-10 14:50:07 +0800

    feat: int variables

---
+++ Variables/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+  public static void main(String[] args) {
+    int people = 20;
+    System.out.println(people);
+
+    int busTickets = people * 2;
+
+    System.out.println(busTickets / 3);
+
+
+  }
+}
