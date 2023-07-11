72a8483f230a9d7714fadd94ac6ad50df26d0aef
catzilla <catzillaorz@outlook.com>
2023-07-11 22:48:37 +0800

    feat: While Vs for

---
+++ Variables/WhileLoops.java
@@ -0,0 +1,13 @@
+/**
+ * When to use While: 
+ * use it when you don't know how many times it will run
+ */
+public class WhileLoops {
+  public static void main(String[] args) {
+    int number = 25;
+    while(number < 30){
+      number++;
+      System.out.println(number);
+    }
+  }
+}
