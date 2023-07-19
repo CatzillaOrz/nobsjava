3ea88ebbf4188056a9001b3fa29b742aee7a316f
catzilla <catzillaorz@outlook.com>
2023-07-19 20:48:31 +0800

    feat: enum practice

--- enum/Main.java
+++ enum/Main.java
@@ -1,16 +1,41 @@
 enum Planet {
-  mercury(1),
-  venus(2),
-  earch(3),
-  mars(4),
-  jupiter(5),
-  saturn(6),
-  uranus(7),
-  neptune(8),
-  pluto(9)
+  MERCURY(1),
+  VENUS(2),
+  EARTH(3),
+  MARS(4),
+  JUPITER(5),
+  SATURN(6),
+  URANUS(7),
+  NEPTUNE(8),
+  PLUTO(9);
 
-    int number;
+  int number;
   Planet (int number){
     this.number = number;
   }
 }
+
+public class Main {
+  
+  public static void main(String[] args) {
+
+    Planet myPlanet = Planet.MARS;
+    canILiveHere(myPlanet);
+    
+    
+  }
+
+  public static void canILiveHere(Planet myPlanet){
+    
+    switch (myPlanet) {
+      case EARTH:
+        System.out.println("You can live here :)"); 
+        break;
+      default:
+        System.out.println("You can't live here...yet ");
+        break;
+    }
+  }
+}
+
+
