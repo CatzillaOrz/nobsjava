2917bf206cbafbd6f196df300110fd88deddb443
catzilla <catzillaorz@outlook.com>
2023-07-19 16:39:07 +0800

    feat: enum

---
+++ enum/Main.java
@@ -0,0 +1,16 @@
+enum Planet {
+  mercury(1),
+  venus(2),
+  earch(3),
+  mars(4),
+  jupiter(5),
+  saturn(6),
+  uranus(7),
+  neptune(8),
+  pluto(9)
+
+    int number;
+  Planet (int number){
+    this.number = number;
+  }
+}
