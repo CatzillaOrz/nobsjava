84af6e9b2b77209f33d4b014274fbfc6c82fcf71
catzilla <catzillaorz@outlook.com>
2023-07-21 17:42:14 +0800

    feat: InnerClasses exercise

--- InnerClasses/Main.java
+++ InnerClasses/Main.java
@@ -1,8 +1,20 @@
 public class Main {
   public static void main(String[] args) {
+    /**
     Outside out = new Outside();
-
     Outside.Inside inside = out.new Inside();
     System.out.println(out.x + inside.y);
+    */
+
+    Greeting greeting = new Greeting(){
+      public void Welcome(){
+        System.out.println("Yo bro! ");
+      }
+    };
+
+    greeting.Welcome();
+
+
   }
+
 }
---
+++ InnerClasses/MyFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+  myFrame(){
+    JButton button1 = new JButton("Button #1");
+    JButton button1 = new JButton("Button #1");
+    JButton button2 = new JButton("Button #3");
+  }
+  
+}
