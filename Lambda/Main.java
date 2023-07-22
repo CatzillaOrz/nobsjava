ca6869ebad94d7db8092a58933c2d0e3f5fc7105
catzilla <catzillaorz@outlook.com>
2023-07-22 09:28:12 +0800

    feat: InnerClass -> lambda exercise

--- Lambda/Main.java
+++ Lambda/Main.java
@@ -2,11 +2,25 @@ public class Main {
   public static void main(String[] args) {
     String name = "Bro";
     char symbol = '!';
+
+    // #1
     MyInterface myInterface = (x, s) ->  {
+      System.out.println("@1");
       System.out.println("Hello world ");
       System.out.println(s);
       System.out.println(x);
     };
+
+    // #2
+    MyInterface myInterfaceOverride = (x,y) ->{
+      System.out.println("@2");
+      System.out.println(x);
+      System.out.println(y);
+    };
+
+    // @1
     myInterface.message(name, symbol);
+    // @2
+    myInterfaceOverride.message(name, symbol);
   }
 }
