919e1d007a39e9601414a4e2a0c6162d4f7e9486
root <root@guest.guest>
2023-07-29 05:16:32 +0000

    feat: QuickSort of BigO

---
+++ BigO/QuickSort/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String args[]){
+		int[] array = {8, 2,5,3,9,4,7,6,1};
+		quickSort(array, 0, array.length-1);
+		for(int i:array){
+			System.out.print(i + " ");
+		}
+	}
+	public static void quickSort(int[] array, int start, int end){
+		if(end <= start ) return;
+		 
+
+	};
+} 
