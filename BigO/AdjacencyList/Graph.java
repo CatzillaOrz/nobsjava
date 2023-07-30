75e542e70794320fe6945f2100a4f8ddfec576a9
catzilla <catzillaorz@outlook.com>
2023-07-30 16:03:44 +0800

    feat: AdjacencyList of Big(O)

--- BigO/AdjacencyList/Graph.java
+++ BigO/AdjacencyList/Graph.java
@@ -9,14 +9,34 @@ public class Graph {
     }
 
     public void addNode(Node node) {
+        LinkedList<Node> currentList = new LinkedList<>();
+        currentList.add(node);
+        alist.add(currentList);
     }
 
     public void addEdge(int src, int dst) {
+        LinkedList<Node> currentList = alist.get(src);
+        Node dstNode = alist.get(dst).get(0);
+        currentList.add(dstNode);
     }
 
     public boolean checkEdge(int src, int dst) {
+        LinkedList<Node> currentList = alist.get(src);
+        Node dstNode = alist.get(dst).get(0);
+        for (Node node : currentList) {
+            if (node == dstNode) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void print() {
+        for (LinkedList<Node> currentList : alist) {
+            for (Node node : currentList) {
+                System.err.print(node.data + " -> ");
+            }
+            System.out.println();
+        }
     }
 }
